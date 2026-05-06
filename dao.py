ffb1d01869d7fe8eed401f20b7904c4727b5bc09
ososdis <oscardisu2@gmail.com>
2026-05-06 15:19:25 +0100

    Update dao.py

---
+++ dao.py
@@ -0,0 +1,14 @@
+# Exercice 1
+## Implémentation des DAOs des modèles DTO suivant le diagramme class_comp.puml
+class UserDAO:
+    def __init__(
+        self,
+    ):
+        pass
+
+
+class EventDAO:
+    def __init__(
+        self,
+    ):
+        pass
